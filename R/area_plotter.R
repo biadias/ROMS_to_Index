c75b097b2f770d86f53f589c033b5c25e7d5ba18
Alberto Rovellini <alberto.rovellini@gmail.com>
2023-04-05 13:43:09 -0700

    Added 300 m isobath

--- R/area_plotter.R
+++ R/area_plotter.R
@@ -8,8 +8,12 @@ nmfs <- st_read("Data/NMFS management area shapefiles/gf95_nmfs.shp")
 nmfs <- nmfs %>% filter(NMFS_AREA %in% c(610,620,630,640,650)) %>% # subset to 610-650
   filter(GF95_NMFS1 %in% c(186,194,259,585,870)) # Removes inter-coastal in SE AK
 
-# NMFS areas clipped to isobaths
-nmfs_clipped <- st_read("Data/Depth trimmed NMFS shapefiles/NMFS610-650.shp")
+# NMFS areas clipped to 1000 m isobath
+nmfs_clipped_1000 <- st_read("Data/Depth trimmed NMFS shapefiles/NMFS610-650.shp")
+
+# NMFS areas clipped to 300 m isobath
+# This is what we currently use for the indices
+nmfs_clipped_300 <- st_read("Data/Depth trimmed NMFS shapefiles 300/NMFS610-650.shp")
 
 # coast
 coast <- maps::map(database = 'worldHires', regions = c('USA','Canada'), plot = FALSE, fill=TRUE)
@@ -23,6 +27,9 @@ coast_sf <- coast %>%
 # view
 ggplot()+
   geom_sf(data = nmfs, color = 'red', fill = NA)+
-  geom_sf(data = nmfs_clipped, color = 'blue', fill = NA)+
-  geom_sf(data = coast_sf, fill = 'grey')
+  geom_sf(data = nmfs_clipped_1000, color = 'blue', fill = NA)+
+  geom_sf(data = nmfs_clipped_300, color = 'orange', fill = NA)+
+  geom_sf(data = coast_sf, fill = 'grey')+
+  theme_bw()
   
+# remember that these are for visualization purpose - the actual indices are computed over ROMS depth
