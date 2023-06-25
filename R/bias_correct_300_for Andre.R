33f365d30e84c4c7519698dad1e54d2ed1315ad4
grantdadams <grantadams60091@gmail.com>
2023-06-25 16:20:09 -0700

    Update the indices for Andre and cleaned up file structure
    Indices are in the output folder

--- R/bias_correct_300_for Andre.R
+++ R/bias_correct_300_for Andre.R
@@ -1,10 +1,13 @@
-# prepare 0-300 m
-library(tidyverse)
+# Alberto Rovellini and Grant Adams
+# Derive indices on average surface and bottom temperature for NMFS mgmt areas 610 to 630 between February and April
+# Using ROMS NEP subset to the 300 m isocline
 
-pacman::p_load(mgcv, dplyr, lubridate, ggplot2, tidyr, sf)
+pacman::p_load(mgcv, dplyr, lubridate, ggplot2, tidyr, sf, tidyverse)
 source("R/Delta_correction.R")
 
-# calc areas
+# ------------------------------------------
+# CALCULATE AREA PER NMFS MGMT AREA
+# ------------------------------------------
 nmfs <- st_read('Data/Depth trimmed NMFS shapefiles 300/NMFS610-650.shp')
 areas <- nmfs %>% rowwise() %>% mutate(areas = st_area(geometry)) %>% ungroup()
 
@@ -25,17 +28,20 @@ areas <- areas %>% group_by(NMFS_AREA) %>% summarise(geometry = st_union(geometr
 # 640 37270389681 [m^2]
 # 650 43952466109 [m^2]
 
+
+# ------------------------------------------
 # Load data averaged across depth and strata
-nep_hind <- read.csv("Data/output_from_loon/nep_avg_hind_300.csv")
+# ------------------------------------------
+nep_hind <- read.csv("Data/NEP_10k_revised_indices/nep_avg_hind_300.csv")
 nep_hind$simulation = "hindcast"
 
-nep_hist <- read.csv("Data/output_from_loon/nep_avg_wb_hist_300.csv")
+nep_hist <- read.csv("Data/NEP_10k_revised_indices/nep_avg_wb_hist_300.csv")
 nep_hist$simulation = "historical"
 
-nep_ssp126 <- read.csv("Data/output_from_loon/nep_avg_wb_ssp126_300.csv")
+nep_ssp126 <- read.csv("Data/NEP_10k_revised_indices/nep_avg_wb_ssp126_300.csv")
 nep_ssp126$simulation = "ssp126"
 
-nep_ssp585 <- read.csv("Data/output_from_loon/nep_avg_wb_ssp585_300.csv")
+nep_ssp585 <- read.csv("Data/NEP_10k_revised_indices/nep_avg_wb_ssp585_300.csv")
 nep_ssp585$simulation = "ssp585"
 
 # Combine in list
@@ -48,10 +54,13 @@ roms_avg_data <- roms_avg_data %>%
     month = lubridate::month(date),
     year = lubridate::year(date))
 
-# Run bias correction for all variables 
-# - rbinds bias-corrected projection to historical run
+
+# ---------------------------------------------------------
+# Run bias correction for all variables  (hind and no hind)
+# ---------------------------------------------------------
+# - - 1) NO HINDCAST SPLICED IN
 # - SSP126
-ssp126_biascorrected <- delta_correction(
+ssp126_biascorrected_nohind <- delta_correction(
   hindcast = roms_avg_data %>% filter(simulation == "hindcast"),
   historical = roms_avg_data %>% filter(simulation == "historical"),
   projection = roms_avg_data %>% filter(simulation == "ssp126"),
@@ -60,7 +69,7 @@ ssp126_biascorrected <- delta_correction(
   include_hindcast = FALSE)
 
 # - SSP585
-ssp585_biascorrected <- delta_correction(
+ssp585_biascorrected_nohind <- delta_correction(
   hindcast = roms_avg_data %>% filter(simulation == "hindcast"),
   historical = roms_avg_data %>% filter(simulation == "historical"),
   projection = roms_avg_data %>% filter(simulation == "ssp585"),
@@ -68,93 +77,120 @@ ssp585_biascorrected <- delta_correction(
   lognormal = FALSE,
   include_hindcast = FALSE)
 
-# # plot differences between the bias-corrected and raw runs
-# ssp585_biascorrected %>%
-#   select(-unit, -value) %>%
-#   left_join(nep_ssp585 %>% 
-#               mutate(year = year(date), month = month(date)) %>%
-#               select(NMFS_AREA, depthclass, value, varname, year, month),
-#             by = c('NMFS_AREA', 'depthclass', 'varname', 'year', 'month')) %>%
-#   filter(between(year, 2035, 2036)) %>%
-#   select(-year, -month) %>%
-#   pivot_longer(c(value, value_dc)) %>%
-#   filter(varname == 'temp' & depthclass %in% c('Surface', 'Bottom') & NMFS_AREA == 610) %>%
-#   ggplot(aes(x = date, y = value, color = name))+
-#   geom_line()+
-#   geom_point()+
-#   theme_bw()+
-#   facet_wrap(depthclass~NMFS_AREA)
+# - - 2) HINDCAST SPLICED IN
+# - SSP126
+ssp126_biascorrected_hind <- delta_correction(
+  hindcast = roms_avg_data %>% filter(simulation == "hindcast"),
+  historical = roms_avg_data %>% filter(simulation == "historical"),
+  projection = roms_avg_data %>% filter(simulation == "ssp126"),
+  ref_yrs = 2000:2014, # Overlap years for historical and hindcast ROMS
+  lognormal = FALSE,
+  include_hindcast = TRUE) # Splice hindcast in
+
+# - SSP585
+ssp585_biascorrected_hind <- delta_correction(
+  hindcast = roms_avg_data %>% filter(simulation == "hindcast"),
+  historical = roms_avg_data %>% filter(simulation == "historical"),
+  projection = roms_avg_data %>% filter(simulation == "ssp585"),
+  ref_yrs = 2000:2014, # Overlap years for historical and hindcast ROMS
+  lognormal = FALSE,
+  include_hindcast = TRUE) # Splice hindcast in
+
+
+# - Plot differences between the bias-corrected and raw runs
+ssp585_biascorrected_nohind %>%
+  select(-unit, -value) %>%
+  left_join(nep_ssp585 %>%
+              mutate(year = year(date), month = month(date)) %>%
+              select(NMFS_AREA, depthclass, value, varname, year, month),
+            by = c('NMFS_AREA', 'depthclass', 'varname', 'year', 'month')) %>%
+  filter(between(year, 2020, 2036)) %>%
+  select(-year, -month) %>%
+  pivot_longer(c(value, value_dc)) %>%
+  filter(varname == 'temp' & depthclass %in% c('Surface', 'Bottom') & NMFS_AREA == 610) %>%
+  ggplot(aes(x = date, y = value, color = name))+
+  geom_line()+
+  geom_point()+
+  theme_bw()+
+  facet_wrap(depthclass~NMFS_AREA)
+
 
 # Extract variables we want
-# surface
-goa_SST_ssp126 <- ssp126_biascorrected %>%
-  filter(varname == "temp" & depthclass == "Surface" & NMFS_AREA %in% c("610", "620")) %>%
+# ---------------------------------------
+# Surface and bottom temperature
+# ---------------------------------------
+# -- 1) No hindcast
+goa_temp_ssp126_nohind <- ssp126_biascorrected_nohind %>%
+  filter(varname == "temp" & depthclass %in% c("Surface", "Bottom") & NMFS_AREA %in% c("610", "620", "630")) %>%
   mutate(simulation = "ssp126") %>%
   pivot_wider(values_from = c(value_dc, value), names_from = NMFS_AREA) %>%
-  mutate(value_dc_610_620 = (value_dc_610 * 57225003746 + value_dc_620 * 62597059226) / (57225003746 + 62597059226)) # Take area weighted mean
+  mutate(value_dc_610_to_630 = (value_dc_610 * 57225003746 + value_dc_620 * 62597059226 + value_dc_630 * 98582220025) / (57225003746 + 62597059226 + 98582220025)) # Take area weighted mean
 
-goa_SST_ssp585 <- ssp585_biascorrected %>%
-  filter(varname == "temp" & depthclass == "Surface" & NMFS_AREA %in% c("610", "620")) %>% 
+goa_temp_ssp585_nohind <- ssp585_biascorrected_nohind %>%
+  filter(varname == "temp" & depthclass %in% c("Surface", "Bottom")  & NMFS_AREA %in% c("610", "620", "630")) %>% 
   mutate(simulation = "ssp585") %>%
   pivot_wider(values_from = c(value_dc, value), names_from = NMFS_AREA) %>%
-  mutate(value_dc_610_620 = (value_dc_610 * 57225003746 + value_dc_620 * 62597059226) / (57225003746 + 62597059226)) # Take area weighted mean
+  mutate(value_dc_610_to_630 = (value_dc_610 * 57225003746 + value_dc_620 * 62597059226 + value_dc_630 * 98582220025) / (57225003746 + 62597059226 + 98582220025)) # Take area weighted mean
+
+goa_temp_610_to_630_nohind <- rbind(goa_temp_ssp126_nohind, goa_temp_ssp585_nohind)
+goa_temp_610_to_630_nohind <- goa_temp_610_to_630_nohind %>% mutate(hind = 'no')
 
 
-# Plot it
-goa_SST_610_620_surface <- rbind(goa_SST_ssp126, goa_SST_ssp585)
-ggplot(goa_SST_610_620_surface, aes(date, value_dc_610_620, colour = simulation)) + geom_line() +
-  ylab("SST (Celsius)") + xlab("Year") + facet_wrap(~simulation)
-
-write.csv(goa_SST_610_620_surface, 'goa_temperature_610_620_surface_300m_NoHindcast.csv', row.names = F)
-
-# bottom
-goa_SST_ssp126 <- ssp126_biascorrected %>%
-  filter(varname == "temp" & depthclass == "Bottom" & NMFS_AREA %in% c("610", "620")) %>%
+# -- 2) Including hindcast
+goa_temp_ssp126_hind <- ssp126_biascorrected_hind %>%
+  filter(varname == "temp" & depthclass %in% c("Surface", "Bottom") & NMFS_AREA %in% c("610", "620", "630")) %>%
   mutate(simulation = "ssp126") %>%
   pivot_wider(values_from = c(value_dc, value), names_from = NMFS_AREA) %>%
-  mutate(value_dc_610_620 = (value_dc_610 * 57225003746 + value_dc_620 * 62597059226) / (57225003746 + 62597059226)) # Take area weighted mean
+  mutate(value_dc_610_to_630 = (value_dc_610 * 57225003746 + value_dc_620 * 62597059226 + value_dc_630 * 98582220025) / (57225003746 + 62597059226 + 98582220025)) # Take area weighted mean
 
-goa_SST_ssp585 <- ssp585_biascorrected %>%
-  filter(varname == "temp" & depthclass == "Bottom" & NMFS_AREA %in% c("610", "620")) %>% 
+goa_temp_ssp585_hind <- ssp585_biascorrected_hind %>%
+  filter(varname == "temp" & depthclass %in% c("Surface", "Bottom") & NMFS_AREA %in% c("610", "620", "630")) %>% 
   mutate(simulation = "ssp585") %>%
   pivot_wider(values_from = c(value_dc, value), names_from = NMFS_AREA) %>%
-  mutate(value_dc_610_620 = (value_dc_610 * 57225003746 + value_dc_620 * 62597059226) / (57225003746 + 62597059226)) # Take area weighted mean
+  mutate(value_dc_610_to_630 = (value_dc_610 * 57225003746 + value_dc_620 * 62597059226 + value_dc_630 * 98582220025) / (57225003746 + 62597059226 + 98582220025)) # Take area weighted mean
+
+goa_temp_610_to_630_hind <- rbind(goa_temp_ssp126_hind, goa_temp_ssp585_hind)
+goa_temp_610_to_630_hind <- goa_temp_610_to_630_hind %>% mutate(hind = 'yes')
 
 
-# Plot it
-goa_SST_610_620_bottom <- rbind(goa_SST_ssp126, goa_SST_ssp585)
-ggplot(goa_SST_610_620_bottom, aes(date, value_dc_610_620, colour = simulation)) + geom_line() +
-  ylab("SST (Celcius)") + xlab("Year") + facet_wrap(~simulation)
+# -- Combine and plot
+goa_temp_610_to_630 <- rbind(goa_temp_610_to_630_hind, goa_temp_610_to_630_nohind)
 
-write.csv(goa_SST_610_620_bottom, 'goa_temperaure_610_620_bottom_300m_NoHindcast.csv', row.names = F)
+ggplot(goa_temp_610_to_630, aes(date, value_dc_610_to_630, colour = simulation)) + geom_line() +
+  ylab("SST (Celsius)") + xlab("Year") + facet_wrap(~simulation + hind + depthclass)
 
+goa_temp_610_to_630 <- goa_temp_610_to_630 %>%
+  arrange(depthclass, simulation, hind, year, month) %>%
+  relocate(hind, .before = varname) %>%
+  relocate(simulation, .before = varname)
 
-# Check against Grant's SST file ------------------------------------------
-
-# dat_grant <- read.csv('610_620_SST.csv')
-# dat_albi <- read.csv('goa_SST_610_620_surface_1000.csv') # try full data first
-# 
-# test <- data.frame('g' = dat_grant$value_dc_610_620, 'a' = dat_albi$value_dc_610_620)
-# test %>% mutate(diff = g - a) %>% pull(diff) %>% abs() %>% max() # 4.9919e-09
-# 
-# ggplot(dat_albi %>% filter(year %in% c(2014:2016)), aes(x = as.Date(date), y = value_dc_610_620))+
-#   geom_line()+
-#   facet_wrap(~simulation)
+write.csv(goa_temp_610_to_630, 'Output/goa_temperature_610_to_630_300m.csv', row.names = F)
 
 
-# Check with and without hindcast baked in --------------------------------
+# ---------------------------------------
+# Calc mean feb to april temperature
+# ---------------------------------------
+goa_temp_610_to_630_FebApril <- goa_temp_610_to_630 %>%
+  filter(month %in% c(2:4)) %>%
+  group_by(year, depthclass, varname, simulation, hind) %>%
+  summarise(mean_value_dc_610_to_630 = mean(value_dc_610_to_630)) %>% 
+  mutate(varname = "Mean temp Feb to April") %>%
+  arrange(depthclass, simulation, hind, year) %>%
+  select(depthclass, hind, simulation, varname, year, mean_value_dc_610_to_630)
 
-dat_hind <- read.csv('goa_temperaure_610_620_bottom_300m_Hindcast.csv')
-dat_no_hind <- read.csv('goa_temperaure_610_620_bottom_300m_NoHindcast.csv')
+# -- Plot
+ggplot(goa_temp_610_to_630_FebApril %>% filter(depthclass == "Surface"), aes(year, mean_value_dc_610_to_630, colour = simulation)) + 
+  geom_line() +
+  ylab("SST (Celsius)") + 
+  xlab("Year") + 
+  facet_wrap(~simulation + hind)
 
-dat_hind <- dat_hind %>% mutate(hind = 'yes')
-dat_no_hind <- dat_no_hind %>% mutate(hind = 'no')
+ggplot(goa_temp_610_to_630_FebApril %>% filter(depthclass == "Bottom"), aes(year, mean_value_dc_610_to_630, colour = simulation)) + 
+  geom_line() +
+  ylab("Bottom temp (Celsius)") + 
+  xlab("Year") + 
+  facet_wrap(~simulation + hind)
 
-datall <- rbind(dat_hind, dat_no_hind)
+# -- Save
+write.csv(goa_temp_610_to_630_FebApril, 'Output/goa_temp_610_to_630_FebApril_300M.csv', row.names = F)
 
-datall %>%
-  filter(between(year, 2010, 2020) & simulation == 'ssp585') %>%
-  ggplot()+
-  geom_line(aes(x = as.Date(date), y = value_dc_610_620, color = hind))+
-  theme_bw()
-  
