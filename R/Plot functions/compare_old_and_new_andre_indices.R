9a5d69443be7ba7e1c00917dc9d5a9f5c71aad10
grantdadams <grantadams60091@gmail.com>
2023-06-26 09:09:15 -0700

    add comparison of andre indices using old vs new ROMS

---
+++ R/Plot functions/compare_old_and_new_andre_indices.R
@@ -0,0 +1,30 @@
+library(tidyverse)
+
+old_goa_temp_610_to_630_FebApril <- read.csv('Output/Old_goa_temp_610_to_630_FebApril_300M.csv')
+revised_goa_temp_610_to_630_FebApril <- read.csv('Output/goa_temp_610_to_630_FebApril_300M.csv')
+
+old_goa_temp_610_to_630_FebApril <- old_goa_temp_610_to_630_FebApril %>%
+  mutate(revised = FALSE)
+
+revised_goa_temp_610_to_630_FebApril <- revised_goa_temp_610_to_630_FebApril %>%
+  mutate(revised = TRUE)
+
+
+goa_temp_610_to_630_FebApril <- rbind(old_goa_temp_610_to_630_FebApril, revised_goa_temp_610_to_630_FebApril)
+
+# Plot
+
+# -- Plot
+# - SST
+ggplot(goa_temp_610_to_630_FebApril %>% filter(depthclass == "Surface" & hind == "yes"), aes(year, mean_value_dc_610_to_630, colour = simulation)) + 
+  geom_line() +
+  ylab("SST (Celsius)") + 
+  xlab("Year") + 
+  facet_wrap(~simulation + revised)
+
+# - Bottom temp
+ggplot(goa_temp_610_to_630_FebApril %>% filter(depthclass == "Bottom" & hind == "yes"), aes(year, mean_value_dc_610_to_630, colour = simulation)) + 
+  geom_line() +
+  ylab("Bottom temp (Celsius)") + 
+  xlab("Year") + 
+  facet_wrap(~simulation + revised)
